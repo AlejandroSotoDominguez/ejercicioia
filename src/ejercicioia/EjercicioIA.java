0185282cc3e8f79e2cd63351b4700fe7a1402e83
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2017-11-09 08:57:04 +0100

    Inicio

---
+++ src/ejercicioia/EjercicioIA.java
@@ -0,0 +1,15 @@
+
+package ejercicioia;
+
+import java.util.Scanner;
+
+public class EjercicioIA {
+
+    public static void main(String[] args) {
+        String nombre;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre:");
+        nombre = sc.next();
+        System.out.println("Hola "+nombre);
+    }   
+}
